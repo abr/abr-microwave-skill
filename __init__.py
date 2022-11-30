d1b4c5e380b2d5b8e83a782ee1c21632a6a4215c
NarsimhaChilkuri <narasimha.chilkuri@gmail.com>
2022-11-30 13:02:55 -0500

    Add basic reheat intent

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from mycroft import MycroftSkill, intent_file_handler
 
 
@@ -5,11 +8,42 @@ class AbrMicrowave(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('microwave.abr.intent')
-    def handle_microwave_abr(self, message):
-        self.speak_dialog('microwave.abr')
+        # Current status of the microwave
+        self.state = {
+            "state": 0,  #  microwave on/off
+            "heat_mode": "medium",  # Literal["low", "medium", "high"], current heat mode
+            "timer": 0.0,  # int, keeps track of the cooking time in seconds
+            "light": 0,  # microwave light on/off
+        }
+        # Avaialble heat/power modes
+        self.heat_modes = ("low", "medium", "high")
+
+    def _extract_and_set_time(self, time_entity):
+        time = "".join([x for x in time_entity if x.isnumeric()])
+        time = int(time)
+        if "second" in time_entity:
+            self.state["timer"] = time
+        elif "minute" in time_entity:
+            self.state["timer"] = int(time * 60)
+        elif "hour" in time_entity:
+            self.state["timer"] = int(time * 3600)
+
+    def _time_display_and_track(self):
+        for i in range(self.state["timer"], -1, -1):
+            print(f"{datetime.timedelta(seconds=i)}", end="\r", flush=True)
+            time.sleep(1)
+            self.state["timer"] -= 1
+        self.acknowledge()
+
+    def initialize(self):
+        self.register_entity_file("time.entity")
+
+    @intent_file_handler("reheat.basic.intent")
+    def handle_reheat_basic(self, message):
+        self.log.info("In REHEAT BASIC handler")
+        self._extract_and_set_time(message.data["time"])
+        self._time_display_and_track()
 
 
 def create_skill():
     return AbrMicrowave()
-
